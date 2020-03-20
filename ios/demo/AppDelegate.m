b37d0710f8d0e4380a9e0c32157798d701e8659a
吴琼 <wuqiongaia@163.com>
2020-03-20 18:06:41 +0800

    备注原生项目

--- ios/demo/AppDelegate.m
+++ ios/demo/AppDelegate.m
@@ -15,17 +15,22 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  //初始化异步批处理桥用于与JavaScript应用程序通信。
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  //载入js项目，并返回由react初始化的根页面
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"demo"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+ //初始化window页面
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  //建立根控制器
   UIViewController *rootViewController = [UIViewController new];
+  //把react初始化的根页面传给根控制器
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
+  //显示页面
   [self.window makeKeyAndVisible];
   return YES;
 }
