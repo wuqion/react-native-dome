5032850ae2afee446be1d03f73c45ec83d2b3d54
吴琼 <wuqiongaia@163.com>
2020-03-22 11:50:24 +0800

    tabs加入图片

--- src/BaseTabNavigator.js
+++ src/BaseTabNavigator.js
@@ -15,6 +15,15 @@ import DetailsScreen from './home/details/DetailsScreen';
 import FindScreen from './find/FindScreen';
 
 import CenterScreen from './center/CenterScreen'
+const tabsImage = {
+    home : require('../images/tabs/on/home.png'),
+    homeSelect : require('../images/tabs/select/home.png'),
+    find : require('../images/tabs/on/danyehuaban.png'),
+    findSelect : require('../images/tabs/select/danyehuaban.png'),
+    center : require('../images/tabs/on/geren.png'),
+    centerSelect : require('../images/tabs/select/geren.png')
+}
+
 
 const Tab = createBottomTabNavigator();
 
@@ -49,24 +58,27 @@ export default class BaseTabNavigator extends React.Component {
     render() {
         return (
             <NavigationContainer>
-                <Tab.Navigator>
+                <Tab.Navigator  tabBarOptions={{
+                    activeTintColor: '#1afa29',
+                    inactiveTintColor: '#999',
+                    }}>
                     <Tab.Screen name="Home" component={Home} options={{
-                        tabBarLabel: '首页',
+                        title: '首页',
                         tabBarIcon: ({ color, size, focused }) => (
-                            <View style={{ backgroundColor: focused ? 'red' : 'blue', height: 20, width: 30 }}></View>
+                            <Image source={focused?tabsImage.home:tabsImage.homeSelect}></Image>
                         ),
 
                     }} />
                     <Tab.Screen name="find" component={Find} options={{
                         tabBarLabel: '发现',
                         tabBarIcon: ({ color, size, focused }) => (
-                            <View style={{ backgroundColor: focused ? 'red' : 'blue', height: 20, width: 30 }}></View>
-                        )
+                            <Image source={focused?tabsImage.find:tabsImage.findSelect}></Image>
+                        ),
                     }}/>
                     <Tab.Screen name="center" component={Center} options={{
                         tabBarLabel:'个人',
                         tabBarIcon:({focused})=>(
-                            <View style={{ backgroundColor: focused ? 'red' : 'blue', height: 20, width: 30 }}></View>
+                            <Image source={focused?tabsImage.center:tabsImage.centerSelect}></Image>
                         )
                     }} />
                 </Tab.Navigator>
